747eb7d36b11ceb10f06e456e584502e898687ec
juan <jpalacio@axletravel.com>
2019-08-28 11:33:07 -0500

    cleaning dataset

---
+++ prepare_dataset.py
@@ -0,0 +1,7 @@
+from imutils import paths
+
+BASE_PATH = "../datasets/chest_xrays/"
+TEST_PATH = BASE_PATH + "train/PNEUMONIA"
+TRAIN_PATH = BASE_PATH + "train/PNEUMONIA"
+
+test_paths = paths.list_images()
